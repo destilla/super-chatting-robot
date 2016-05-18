12cb4f6ccf20f2d83b14b94466336a2a62a90fba
Destillat <alex.guerrera@gmail.com>
2016-05-18 11:13:51 -0700

    First commit

---
+++ rosie.js
@@ -0,0 +1,99 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+channels : ["#channel"],
+***REMOVED***
+***REMOVED***
+***REMOVED***
+nick : "BotName",
+password : "oauth:"
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***);
+
+
+***REMOVED***
+if(who == "BotName"){
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***);
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***);
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***);
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+  ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+  ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+  headers: { 'Content-Type': 'application/json' ***REMOVED***,
+***REMOVED***
+***REMOVED***
+    needle.get('https://api.twitch.tv/kraken/streams/',opts, function(err,resp) {
+***REMOVED***
+***REMOVED***
+***REMOVED***
+      
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    ***REMOVED***);
+  ***REMOVED***
+
+  if(text === '!dickvanhype'){
+***REMOVED***
+  ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+  ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+  ***REMOVED***
+***REMOVED***);
